71e863c2c9027ee53f66204e3148c1bd78f5f7e4
taiiii123 <taiiii88UQTG@gmail.com>
2025-11-11 22:00:11 +0900

    変更コメントを追加

--- demo/src/main/java/com/example/Handler.java
+++ demo/src/main/java/com/example/Handler.java
@@ -11,6 +11,7 @@ public class Handler implements RequestHandler<Map<String, String>, String> {
     public String handleRequest(Map<String, String> event, Context context) {
         String name = event.getOrDefault("name", "World");
         return String.format("Hello, %s!", name);
+        // 変更コメントを追加
     }
 
     public String processMessage(String message) {
